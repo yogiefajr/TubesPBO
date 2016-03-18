d82885aabc4239e5d479372b046e6e11dd7a0f16
Yogie fajar <Yogie fajar@DESKTOP-896KLHG>
2016-03-18 17:32:01 +0700

    tulisan yogie

---
+++ TubesPBO/src/tubespbo/Orang.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubespbo;
+
+/**
+ *
+ * @author Yogie fajar
+ */
+public class Orang {
+    private String nama;
+    private String id;
+
+    public privat getString() {
+        return String;
+    }
+
+    public void setString(privat String) {
+        this.String = String;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+    
+}
