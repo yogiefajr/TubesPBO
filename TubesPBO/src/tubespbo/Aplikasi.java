776b1369b909ed93c179a8af9ded6d92974c2af7
User <User@User-PC>
2016-03-28 19:10:20 +0800

    update class matakuliah, tubespbo, sama aplikasi yaa #salambaper

---
+++ TubesPBO/src/tubespbo/Aplikasi.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubespbo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author User
+ */
+public class Aplikasi {
+    private List<Orang> daftarOrang = new ArrayList<>();
+    private Kelas[] daftarKelas;
+    private Matakuliah[] daftarMatakuliah;
+    
+    public void addDosen (Dosen newDosen) {
+        daftarOrang.add(newDosen);
+    }
+    
+    public void addMhasiswa (Mahasiswa newMhs) {
+        daftarOrang.add(newMhs);
+    }
+    
+    public Mahasiswa getMahasiswa (String id) {
+        for ( int i = 1 ; i <= daftarOrang.size() ; i++ ) {
+            if ( daftarOrang.get(i).getId() == id ) {
+                return (Mahasiswa) daftarOrang.get(i);
+            }
+        }
+        return null;
+    }
+    
+    public void deleteMahasiswa (String id) {
+        for ( int i = 1 ; i <= daftarOrang.size() ; i++ ) {
+            if ( daftarOrang.get(i).getId() == id ) {
+                daftarOrang.remove(i);
+            }
+        }
+    }
+    
+    public void menuSatu () {
+        
+    }
+    
+    public void menuDua () {
+        
+    }
+    
+    public void mainMenu () {
+        
+    }
+}
